543116dc1d643ec4cafda710254076aa2ce925d6
Jeree <gunnixe@gmail.com>
2016-11-08 12:29:14 +1100

    mod

--- examples/TMR/add/config.tcl
+++ examples/TMR/add/config.tcl
@@ -1,10 +1,10 @@
 source ../config.tcl
 
-set_parameter LOCAL_RAMS 1
-loop_pipeline "loop"
+#set_parameter LOCAL_RAMS 1
+#loop_pipeline "loop"
 
-set_parameter TMR 1
-set_parameter VOTER_MODE 4
+set_parameter TMR 0
+set_parameter VOTER_MODE 0
 # VOTER_MODE 0=output only,
 #            1=all reigisters,
 #            2=dest signal(fanin, phi),
